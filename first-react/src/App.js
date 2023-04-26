a72bdd22c0edf4ed1aab90846c5cb65699f3c52a
ZzuuCkk <zura.dzidzaria@gmail.com>
2023-04-26 22:28:08 +0400

    added

---
+++ first-react/src/App.js
@@ -0,0 +1,13 @@
+import './custom.css';
+import Player from'./player/player.js';
+
+function App() {
+return (
+  <div className='player-div'>
+    <h1> video player</h1>
+    <Player/>
+  </div>
+);
+}
+
+export default App;
